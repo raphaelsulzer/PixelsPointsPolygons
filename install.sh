0efc987c37b479d7c289cd44e3bf12277398807d
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-03-11 18:44:45 +0100

    add host to conf

--- install.sh
+++ install.sh
@@ -85,6 +85,7 @@ pip install timm
 pip install matplotlib
 pip install albumentations
 pip install shapely
+pip install hydra-core
 
 # # dependencies
 # # pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cu118
