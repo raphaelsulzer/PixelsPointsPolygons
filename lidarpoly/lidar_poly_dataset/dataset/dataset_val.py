fee0fe1de923ad3d56ee0dccbdd6af36d85db519
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-03-12 11:35:10 +0100

    fix imports

--- lidarpoly/lidar_poly_dataset/dataset/dataset_val.py
+++ lidarpoly/lidar_poly_dataset/dataset/dataset_val.py
@@ -1,4 +1,4 @@
-from lidar_poly_dataset import DefaultDataset
+from lidar_poly_dataset.dataset import DefaultDataset
 
 class ValDataset(DefaultDataset):
     def __init__(self,dataset_dir,**kwargs):
