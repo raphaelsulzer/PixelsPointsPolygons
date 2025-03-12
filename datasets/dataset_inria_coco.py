d000833690e3117066ee880ad5fafebc90d52d5d
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-03-12 18:53:01 +0100

    using ori augmentation for lidarpoly

--- datasets/dataset_inria_coco.py
+++ datasets/dataset_inria_coco.py
@@ -3,7 +3,6 @@ import numpy as np
 import os
 from os import path as osp
 from pycocotools.coco import COCO
-from config import CFG
 
 import torch
 from torch.utils.data import Dataset
@@ -11,23 +10,12 @@ from torch.nn.utils.rnn import pad_sequence
 
 
 class InriaCocoDatasetTrain(Dataset):
-    # def __init__(self, dataset_dir, transform=None, tokenizer=None, shuffle_tokens=False):
-    #     image_dir = osp.join(dataset_dir, "images")
-    #     self.image_dir = image_dir
-    #     self.annotations_path = osp.join(dataset_dir, "annotation.json")
-    #     self.transform = transform
-    #     self.tokenizer = tokenizer
-    #     self.shuffle_tokens = shuffle_tokens
-    #     # self.images = os.listdir(self.image_dir)
-    #     self.coco = COCO(self.annotations_path)
-    #     # self.image_ids = self.coco.getImgIds(catIds=self.coco.getCatIds())
-    #     self.images = [file for file in os.listdir(self.image_dir) if osp.isfile(osp.join(self.image_dir, file))]
-    #     self.image_ids = [int(im.split('-')[-1].split('.')[0]) for im in self.images if im.split('-')[0] not in ['kitsap4', 'kitsap5']]
-        
-    
+
     def __init__(self, cfg, transform=None, tokenizer=None):
         super().__init__()
 
+
+        self.cfg = cfg
         self.image_dir = cfg.dataset.train.images
 
         self.transform = transform
@@ -39,7 +27,10 @@ class InriaCocoDatasetTrain(Dataset):
         # self.image_ids = self.coco.getImgIds(catIds=self.coco.getCatIds())
         self.images = [file for file in os.listdir(self.image_dir) if osp.isfile(osp.join(self.image_dir, file))]
         self.image_ids = [int(im.split('-')[-1].split('.')[0]) for im in self.images if im.split('-')[0] not in ['kitsap4', 'kitsap5']]
-    
+
+        print(f"Loaded {len(self.coco.anns.items())} annotations from {cfg.dataset.train.annotations}")
+        
+        a=5
     
 
     def __len__(self):
@@ -123,108 +114,8 @@ class InriaCocoDatasetTrain(Dataset):
         plt.show()
 
 
-    # def __getitem__(self, index):
-    #     n_vertices = CFG.N_VERTICES
-    #     img_id = self.image_ids[index]
-    #     img = self.coco.loadImgs(img_id)[0]
-    #     img_path = osp.join(self.image_dir, img["file_name"])
-    #     ann_ids = self.coco.getAnnIds(imgIds=img['id'])
-    #     annotations = self.coco.loadAnns(ann_ids)  # annotations of all instances in an image.
-
-    #     image = np.array(Image.open(img_path).convert("RGB"))
-
-    #     mask = np.zeros((img['width'], img['height']))
-    #     corner_coords = []
-    #     corner_mask = np.zeros((img['width'], img['height']), dtype=np.float32)
-    #     perm_matrix = np.zeros((n_vertices, n_vertices), dtype=np.float32)
-    #     point_ids = []
-    #     point_id = 0
-    #     for ins in annotations:
-    #         segmentations = ins['segmentation']
-    #         for i, segm in enumerate(segmentations):
-    #             segm = np.array(segm).reshape(-1, 2)
-    #             segm[:, 0] = np.clip(segm[:, 0], 0, img['width'] - 1)
-    #             segm[:, 1] = np.clip(segm[:, 1], 0, img['height'] - 1)
-    #             points = segm[:-1]
-    #             corner_coords.extend(points.tolist())
-    #             mask += self.coco.annToMask(ins)
-    #             point_ids.extend([point_id]*len(points))
-    #             point_id+=1
-    #     mask = mask / 255. if mask.max() == 255 else mask
-    #     mask = np.clip(mask, 0, 1)
-
-    #     corner_coords = np.flip(np.round(corner_coords, 0), axis=-1).astype(np.int32)
-    #     # corner_coords = np.round(corner_coords, 0).astype(np.int32)
-
-    #     if len(corner_coords) > 0.:
-    #         corner_mask[corner_coords[:, 0], corner_coords[:, 1]] = 1.
-
-    #     ############# START: Generate gt permutation matrix. #############
-    #     v_count = 0
-
-    #     # if len(corner_coords) > 0:
-    #     #     # self.debug_vis(np.flip(corner_coords, axis=-1), point_ids, image/255.0)
-    #     #     self.debug_vis(corner_coords, point_ids, image/255.0)
-
-    #     for ins in annotations:
-    #         segmentations = ins['segmentation']
-
-    #         for idx, segm in enumerate(segmentations):
-    #             segm = np.array(segm).reshape(-1, 2)
-    #             points = segm[:-1]
-    #             for i in range(len(points)):
-    #                 j = (i + 1) % len(points)
-    #                 if v_count+i > n_vertices - 1 or v_count+j > n_vertices-1:
-    #                     break
-    #                 perm_matrix[v_count+i, v_count+j] = 1.
-    #             v_count += len(points)
-
-    #     for i in range(v_count, n_vertices):
-    #         perm_matrix[i, i] = 1.
-
-    #     # Workaround for open contours:
-    #     for i in range(n_vertices):
-    #         row = perm_matrix[i, :]
-    #         col = perm_matrix[:, i]
-    #         if np.sum(row) == 0 or np.sum(col) == 0:
-    #             perm_matrix[i, i] = 1.
-    #     perm_matrix = torch.from_numpy(perm_matrix)
-    #     ############# END: Generate gt permutation matrix. #############
-
-    #     masks = [mask, corner_mask]
-
-    #     if len(corner_coords) > CFG.N_VERTICES:
-    #         corner_coords = corner_coords[:CFG.N_VERTICES]
-
-    #     if self.transform is not None:
-    #         augmentations = self.transform(image=image, masks=masks, keypoints=corner_coords.tolist())
-    #         image = augmentations['image']
-    #         mask = augmentations['masks'][0]
-    #         corner_mask = augmentations['masks'][1]
-    #         corner_coords = np.array(augmentations['keypoints'])
-
-    #         # if len(corner_coords) > 0:
-    #         #     # self.debug_vis(corner_coords, point_ids, augmented_image)
-    #         #     augmented_image = (image.permute(1, 2, 0).numpy() * 255).astype(np.uint8)
-    #         #     self.debug_vis(corner_coords, point_ids, augmented_image)
-    #         #     self.debug_vis_mask(mask)
-
-    #     if self.tokenizer is not None:
-    #         coords_seqs, rand_idxs = self.tokenizer(corner_coords, shuffle=self.shuffle_tokens)
-    #         coords_seqs = torch.LongTensor(coords_seqs)
-    #         if self.shuffle_tokens:
-    #             perm_matrix = self.shuffle_perm_matrix_by_indices(perm_matrix, rand_idxs)
-    #     else:
-    #         coords_seqs = corner_coords
-
-
-
-    #     return image, mask[None, ...], corner_mask[None, ...], coords_seqs, perm_matrix, torch.tensor([img['id']])
-
-
-
     def __getitem__(self, index):
-        n_vertices = CFG.N_VERTICES
+        n_vertices = self.cfg.n_vertices
         img_id = self.image_ids[index]
         img = self.coco.loadImgs(img_id)[0]
         img_path = osp.join(self.image_dir, img["file_name"])
