0f7b1261a7ce40f01a9d8b971091e3533c8fafc4
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-03-13 16:55:01 +0100

    started adding lidar support to pix2poly

--- lidarpoly/lidar_poly_dataset/dataset/__init__.py
+++ lidarpoly/lidar_poly_dataset/dataset/__init__.py
@@ -1,3 +1,4 @@
 from .dataset_default import *
 from .dataset_train import TrainDataset
 from .dataset_val import ValDataset
+from .collate_funcs import *
--- lidarpoly/lidar_poly_dataset/dataset/dataset_default.py
+++ lidarpoly/lidar_poly_dataset/dataset/dataset_default.py
@@ -2,7 +2,7 @@ import cv2
 import random
 import numpy as np
 import os
-import copclib as copc
+import laspy
 import logging
 from PIL import Image
 import torch
@@ -56,38 +56,171 @@ class DefaultDataset(Dataset):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-
-    def load_lidar_points(self, lidar_file_name, img_info, z_scale=(0, 512)):
+    def __len__(self):
+        return self.num_samples
+    
+    def load_lidar_points(self, lidar_file_name, img_info, z_scale=(0, 100)):
 
         if os.path.isfile(lidar_file_name):
-            # Create a reader object
-            reader = copc.FileReader(lidar_file_name)
-            # Get the node metadata from the hierarchy
-            node = reader.FindNode(copc.VoxelKey(0, 0, 0, 0))
-            # Fetch the points of a node
-            points = reader.GetPoints(node)
+            
+            las = laspy.read(lidar_file_name)
 
-            points = np.stack([points.x, points.y, points.z], axis=1)
+            points = np.vstack((las.x, las.y, las.z)).transpose()
+
             points[:, :2] = (points[:, :2] - img_info['top_left']) / img_info.get('res_x', 0.25)
             points[:, 1] = img_info['height'] - points[:, 1]
 
-            # scale z vals to [0,512]
+            # scale z vals to [0,100]
             scaler = MinMaxScaler(feature_range=z_scale)  # Change range as needed
             points[:, -1] = scaler.fit_transform(points[:, -1].reshape(-1, 1)).squeeze()
+            
+            points = points.astype(np.float32)
 
         else:
-            self.logger.debug(f'Lidar file {lidar_file_name} missing. Generating random point cloud.')
+            raise FileExistsError(f"Lidar file {lidar_file_name} missing.")
+            # self.logger.debug(f'Lidar file {lidar_file_name} missing. Generating random point cloud.')
 
-            n = 5
-            # make a random point cloud
-            x = np.random.uniform(0, img_info['width'], n)
-            y = np.random.uniform(0, img_info['height'], n)
-            z = np.random.uniform(z_scale[0], z_scale[1], n)
-            points = np.vstack((x, y, z)).T
+            # n = 5
+            # # make a random point cloud
+            # x = np.random.uniform(0, img_info['width'], n)
+            # y = np.random.uniform(0, img_info['height'], n)
+            # z = np.random.uniform(z_scale[0], z_scale[1], n)
+            # points = np.vstack((x, y, z)).T
 
         return points
 
-    def make_annotations(self, ann_coco, height, width):
+    def get_image_file(self, coco_info):
+        filename = os.path.join(self.dataset_dir, coco_info['image_path'])
+        if not os.path.isfile(filename):
+            raise FileNotFoundError(filename)
+        return filename
+
+    def get_lidar_file(self, coco_info):
+        filename = os.path.join(self.dataset_dir, coco_info['lidar_path'])
+        if not os.path.isfile(filename):
+            raise FileNotFoundError(filename)
+        return filename
+
+    def __getitem__(self, idx):
+
+        if self.model_type == 'hisup':
+            pass
+        elif self.model_type == 'pix2poly':
+            return self.__getitem__pix2poly(idx)
+        else:
+            raise NotImplementedError(f"Model type {self.model_type} not implemented.")
+
+    def __getitem__pix2poly(self, index):
+
+        img_id = self.tile_ids[index]
+        img_info = self.coco.loadImgs(img_id)[0]
+        ann_ids = self.coco.getAnnIds(imgIds=img_info['id'])
+        annotations = self.coco.loadAnns(ann_ids)  # annotations of all instances in an image.
+
+        # load image
+        if self.use_images:
+            filename = self.get_image_file(img_info)
+            image = np.array(Image.open(filename).convert("RGB"))
+        else:
+            image = None
+
+        # load lidar
+        if self.use_lidar:
+            filename = self.get_lidar_file(img_info)
+            lidar = self.load_lidar_points(filename, img_info)
+        else:
+            lidar = None
+
+        mask = np.zeros((img_info['width'], img_info['height']))
+        corner_coords = []
+        corner_mask = np.zeros((img_info['width'], img_info['height']), dtype=np.float32)
+        perm_matrix = np.zeros((self.cfg.model.tokenizer.n_vertices, self.cfg.model.tokenizer.n_vertices), dtype=np.float32)
+        point_ids = []
+        point_id = 0
+        for ins in annotations:
+            segmentations = ins['segmentation']
+            for i, segm in enumerate(segmentations):
+                segm = np.array(segm).reshape(-1, 2)
+                segm[:, 0] = np.clip(segm[:, 0], 0, img_info['width'] - 1)
+                segm[:, 1] = np.clip(segm[:, 1], 0, img_info['height'] - 1)
+                points = segm[:-1]
+                corner_coords.extend(points.tolist())
+                mask += self.coco.annToMask(ins)
+                point_ids.extend([point_id] * len(points))
+                point_id += 1
+        mask = mask / 255. if mask.max() == 255 else mask
+        mask = np.clip(mask, 0, 1)
+
+        ### commented the line below, because I think it makes the polygon vertices axis order incoorect
+        corner_coords = np.flip(np.round(corner_coords, 0), axis=-1).astype(np.int32)
+        # corner_coords = np.round(corner_coords, 0).astype(np.int32)
+
+        if len(corner_coords) > 0.:
+            corner_mask[corner_coords[:, 0], corner_coords[:, 1]] = 1.
+
+        ############# START: Generate gt permutation matrix. #############
+        v_count = 0
+        for ins in annotations:
+            segmentations = ins['segmentation']
+            for idx, segm in enumerate(segmentations):
+                segm = np.array(segm).reshape(-1, 2)
+                points = segm[:-1]
+                for i in range(len(points)):
+                    j = (i + 1) % len(points)
+                    if v_count + i > self.cfg.model.tokenizer.n_vertices - 1 or v_count + j > self.cfg.model.tokenizer.n_vertices - 1:
+                        break
+                    perm_matrix[v_count + i, v_count + j] = 1.
+                v_count += len(points)
+
+        for i in range(v_count, self.cfg.model.tokenizer.n_vertices):
+            perm_matrix[i, i] = 1.
+
+        # Workaround for open contours:
+        for i in range(self.cfg.model.tokenizer.n_vertices):
+            row = perm_matrix[i, :]
+            col = perm_matrix[:, i]
+            if np.sum(row) == 0 or np.sum(col) == 0:
+                perm_matrix[i, i] = 1.
+        perm_matrix = torch.from_numpy(perm_matrix)
+        ############# END: Generate gt permutation matrix. #############
+
+        masks = [mask, corner_mask]
+
+        if len(corner_coords) > self.cfg.model.tokenizer.n_vertices:
+            corner_coords = corner_coords[:self.cfg.model.tokenizer.n_vertices]
+
+        if self.transform is not None:
+            augmentations = self.transform(image=image, masks=masks, keypoints=corner_coords.tolist())
+            image = augmentations['image']
+            mask = augmentations['masks'][0]
+            corner_mask = augmentations['masks'][1]
+            corner_coords = np.array(augmentations['keypoints'])
+
+            if self.cfg.use_lidar:
+                lidar = torch.from_numpy(lidar)
+            
+            # if len(corner_coords) > 0:
+            #     # self.debug_vis(corner_coords, point_ids, augmented_image)
+            #     augmented_image = (image.permute(1, 2, 0).numpy() * 255.0).astype(np.uint8)
+            #     self.debug_vis(np.flip(corner_coords,axis=-1), point_ids, augmented_image)
+            #     self.debug_vis_mask(mask)
+            #     self.debug_vis_mask(corner_mask)
+            #     a=5
+
+        if self.tokenizer is not None:
+            coords_seqs, rand_idxs = self.tokenizer(corner_coords, shuffle=self.cfg.model.tokenizer.shuffle_tokens)
+            coords_seqs = torch.LongTensor(coords_seqs)
+            if self.cfg.model.tokenizer.shuffle_tokens:
+                perm_matrix = self.shuffle_perm_matrix_by_indices(perm_matrix, rand_idxs)
+        else:
+            coords_seqs = corner_coords
+
+        return image, lidar, mask[None, ...], corner_mask[None, ...], coords_seqs, perm_matrix, torch.tensor([img_info['id']])
+
+
+
+
+    def make_hisup_annotations(self, ann_coco, height, width):
 
         ann = {
             'junctions': [],
@@ -157,162 +290,4 @@ class DefaultDataset(Dataset):
             ann['juncs_index'] = np.asarray([0])
 
         return ann
-
-
-    def get_image_file(self, coco_info):
-        filename = os.path.join(self.dataset_dir, coco_info['image_path'])
-        if not os.path.isfile(filename):
-            raise FileNotFoundError(filename)
-        return filename
-
-    def get_lidar_file(self, coco_info):
-        filename = os.path.join(self.dataset_dir, coco_info['lidar_path'])
-        if not os.path.isfile(filename):
-            raise FileNotFoundError(filename)
-        return filename
-
-    def __getitem__(self, idx):
-
-        if self.model_type == 'hisup':
-            pass
-        elif self.model_type == 'pix2poly':
-            return self.__getitem__pix2poly(idx)
-        else:
-            raise NotImplementedError(f"Model type {self.model_type} not implemented.")
-
-    def __getitem__pix2poly(self, index):
-
-        img_id = self.tile_ids[index]
-        img_info = self.coco.loadImgs(img_id)[0]
-        ann_ids = self.coco.getAnnIds(imgIds=img_info['id'])
-        annotations = self.coco.loadAnns(ann_ids)  # annotations of all instances in an image.
-
-        # load image
-        if self.use_images:
-            filename = self.get_image_file(img_info)
-            image = np.array(Image.open(filename).convert("RGB"))
-        else:
-            image = None
-
-        # load lidar
-        if self.use_lidar:
-            filename = self.get_lidar_file(img_info)
-            points = self.load_lidar_points(filename, img_info)
-        else:
-            points = None
-
-        mask = np.zeros((img_info['width'], img_info['height']))
-        corner_coords = []
-        corner_mask = np.zeros((img_info['width'], img_info['height']), dtype=np.float32)
-        perm_matrix = np.zeros((self.cfg.model.tokenizer.n_vertices, self.cfg.model.tokenizer.n_vertices), dtype=np.float32)
-        point_ids = []
-        point_id = 0
-        for ins in annotations:
-            segmentations = ins['segmentation']
-            for i, segm in enumerate(segmentations):
-                segm = np.array(segm).reshape(-1, 2)
-                segm[:, 0] = np.clip(segm[:, 0], 0, img_info['width'] - 1)
-                segm[:, 1] = np.clip(segm[:, 1], 0, img_info['height'] - 1)
-                points = segm[:-1]
-                corner_coords.extend(points.tolist())
-                mask += self.coco.annToMask(ins)
-                point_ids.extend([point_id] * len(points))
-                point_id += 1
-        mask = mask / 255. if mask.max() == 255 else mask
-        mask = np.clip(mask, 0, 1)
-
-        ### commented the line below, because I think it makes the polygon vertices axis order incoorect
-        corner_coords = np.flip(np.round(corner_coords, 0), axis=-1).astype(np.int32)
-        # corner_coords = np.round(corner_coords, 0).astype(np.int32)
-
-
-        if len(corner_coords) > 0.:
-            corner_mask[corner_coords[:, 0], corner_coords[:, 1]] = 1.
-
-        # if len(corner_coords) > 0:
-        #     self.debug_vis(np.flip(corner_coords,axis=-1),point_ids,image/255.0)
-        #     self.debug_vis_mask(mask)
-        #     self.debug_vis_mask(corner_mask)
-        #     q=4
-
-        ############# START: Generate gt permutation matrix. #############
-        v_count = 0
-        for ins in annotations:
-            segmentations = ins['segmentation']
-            for idx, segm in enumerate(segmentations):
-                segm = np.array(segm).reshape(-1, 2)
-                points = segm[:-1]
-                for i in range(len(points)):
-                    j = (i + 1) % len(points)
-                    if v_count + i > self.cfg.model.tokenizer.n_vertices - 1 or v_count + j > self.cfg.model.tokenizer.n_vertices - 1:
-                        break
-                    perm_matrix[v_count + i, v_count + j] = 1.
-                v_count += len(points)
-
-        for i in range(v_count, self.cfg.model.tokenizer.n_vertices):
-            perm_matrix[i, i] = 1.
-
-        # Workaround for open contours:
-        for i in range(self.cfg.model.tokenizer.n_vertices):
-            row = perm_matrix[i, :]
-            col = perm_matrix[:, i]
-            if np.sum(row) == 0 or np.sum(col) == 0:
-                perm_matrix[i, i] = 1.
-        perm_matrix = torch.from_numpy(perm_matrix)
-        ############# END: Generate gt permutation matrix. #############
-
-        masks = [mask, corner_mask]
-
-        if len(corner_coords) > self.cfg.model.tokenizer.n_vertices:
-            corner_coords = corner_coords[:self.cfg.model.tokenizer.n_vertices]
-
-        if self.transform is not None:
-            augmentations = self.transform(image=image, masks=masks, keypoints=corner_coords.tolist())
-            image = augmentations['image']
-            mask = augmentations['masks'][0]
-            corner_mask = augmentations['masks'][1]
-            corner_coords = np.array(augmentations['keypoints'])
-
-            # if len(corner_coords) > 0:
-            #     # self.debug_vis(corner_coords, point_ids, augmented_image)
-            #     augmented_image = (image.permute(1, 2, 0).numpy() * 255.0).astype(np.uint8)
-            #     self.debug_vis(np.flip(corner_coords,axis=-1), point_ids, augmented_image)
-            #     self.debug_vis_mask(mask)
-            #     self.debug_vis_mask(corner_mask)
-            #     a=5
-
-        if self.tokenizer is not None:
-            coords_seqs, rand_idxs = self.tokenizer(corner_coords, shuffle=self.cfg.model.tokenizer.shuffle_tokens)
-            coords_seqs = torch.LongTensor(coords_seqs)
-            if self.cfg.model.tokenizer.shuffle_tokens:
-                perm_matrix = self.shuffle_perm_matrix_by_indices(perm_matrix, rand_idxs)
-        else:
-            coords_seqs = corner_coords
-
-        return image, mask[None, ...], corner_mask[None, ...], coords_seqs, perm_matrix, torch.tensor([img_info['id']])
-
-    def __len__(self):
-        return self.num_samples
-
-# def collate_fn(batches, use_lidar, use_images):
-#     if use_images and not use_lidar:
-#         return (default_collate([b[0] for b in batches]), None, [b[2] for b in batches])
-
-#     elif not use_images and use_lidar:
-#         pcds = []
-#         for batch in batches:
-#             pcds.append(batch[1][0, :, :])
-
-#         return (None, pcds, [b[2] for b in batches])
-
-#     elif use_images and use_lidar:
-#         pcds = []
-#         for batch in batches:
-#             pcds.append(batch[1][0, :, :])
-
-#         return (default_collate([b[0] for b in batches]),
-#                 pcds,
-#                 [b[2] for b in batches])
-#     else:
-#         raise NotImplementedError("You must either activate 'use_images' or 'use_lidar'")
 
