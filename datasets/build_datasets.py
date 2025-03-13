6284bc488ef3c2d1ffd4621954161fbedb87f7c2
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-03-13 10:38:03 +0100

    fix inria dataset after removing CFG

--- datasets/build_datasets.py
+++ datasets/build_datasets.py
@@ -128,13 +128,25 @@ def get_val_loader_lidarpoly(cfg,tokenizer):
 def get_train_loader_inria(cfg,tokenizer):
     from datasets.dataset_inria_coco import InriaCocoDatasetTrain, collate_fn
 
-    train_transforms = A.ReplayCompose([
-        A.D4(p=1.0),
-        A.Resize(height=cfg.model.input_height, width=cfg.model.input_width),
-        A.ColorJitter(),
-        A.Normalize(mean=[0.0, 0.0, 0.0], std=[1.0, 1.0, 1.0], max_pixel_value=255.0),
-        ToTensorV2(),
-    ],
+    ### ORIGINAL
+    train_transforms = A.Compose(
+        [
+            A.Affine(rotate=[-360, 360], fit_output=True, p=0.8),  # scaled rotations are performed before resizing to ensure rotated and scaled images are correctly resized.
+            A.Resize(height=cfg.model.input_height, width=cfg.model.input_width),
+            A.RandomRotate90(p=1.),
+            A.RandomBrightnessContrast(p=0.5),
+            A.ColorJitter(),
+            A.ToGray(p=0.4),
+            A.GaussNoise(),
+            # ToTensorV2 of albumentations doesn't divide by 255 like in PyTorch,
+            # it is done inside Normalize function.
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0
+            ),
+            ToTensorV2(),
+        ],
         keypoint_params=A.KeypointParams(format='yx', remove_invisible=False)
     )
         
@@ -189,10 +201,11 @@ def get_val_loader_inria(cfg,tokenizer):
         indices = list(range(cfg.dataset.subset))
         val_ds = Subset(val_ds, indices)
     
+        
     if cfg.multi_gpu:
-        val_sampler = DistributedSampler(dataset=val_ds, shuffle=False)
+        val_sampler = {"sampler":DistributedSampler(dataset=val_ds, shuffle=True)}
     else:
-        val_sampler = None
+        val_sampler = {"shuffle":True}
         
     val_loader = DataLoader(
         val_ds,
---
+++ slurm/README.md
@@ -0,0 +1,6 @@
+# Jean-Zay doc
+http://www.idris.fr/media/eng/ia/guide_nouvel_utilisateur_ia-eng.pdf
+
+## cheatsheet
+http://www.idris.fr/media/su/idrischeatsheet.pdf
+
