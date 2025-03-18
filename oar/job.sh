6f62a1e7de6d9bed07229bc6289c06d443408825
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-03-18 08:41:14 +0100

    fix missing checkpoint path creation

--- oar/job.sh
+++ oar/job.sh
@@ -14,5 +14,5 @@ nvidia-smi
 module load conda
 conda activate ppp
 python3 -c "import torch; print(torch.cuda.is_available()); print(torch.cuda.get_device_name(0))";
-torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true dataset=lidarpoly experiment_name=lidarpoly_D4_aug_bs2x32 checkpoint=null model.batch_size=32
+torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true dataset=lidarpoly experiment_name=lidarpoly_D4_aug_bs2x64 checkpoint=null model.batch_size=64
 
