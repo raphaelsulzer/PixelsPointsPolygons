53b871c0fea1ec8de1becf235300964cd9133866
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-03-18 20:34:47 +0100

    fix some bugs in validation

--- slurm/README.md
+++ slurm/README.md
@@ -4,3 +4,18 @@ http://www.idris.fr/media/eng/ia/guide_nouvel_utilisateur_ia-eng.pdf
 ## cheatsheet
 http://www.idris.fr/media/su/idrischeatsheet.pdf
 
+## Show job queue for user.
+squeue -lu $USER
+## Show pending jobs and the estimated start time for user idrisid
+squeue -u $USER --start
+## Display control states for a job
+scontrol show job jobid 
+## Display control states for a node
+scontrol show node nodeid 
+scancel jobid Delete the job with jobid
+sinfo Display the system settings
+sacct Display job accounting information
+sbatch filename Submit a batch script filename
+salloc Obtain an interactive job allocation
+srun execfile Obtain a job allocation and execute
+an application
--- slurm/job.sh
+++ slurm/job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --account=cso@v100
-#SBATCH --job-name=p2p_lidarpoly_ori_aug  # Job name
+#SBATCH --job-name=image_only_bs4x16  # Job name
 #SBATCH --output=./slurm/output.log       # Standard output and error log
 #SBATCH --error=./slurm/error.log         # Error log
 #SBATCH --nodes=1 # reserve 1 node
 #SBATCH --ntasks=4 # reserve 4 tasks (or processes)
 #SBATCH --gres=gpu:4              # Request 2 GPUs
 #SBATCH --cpus-per-task=16         # Request 8 CPU cores
-#SBATCH --qos=qos_gpu-dev # QoS
+#SBATCH --qos=qos_gpu-t3 # QoS
 #SBATCH --time=16:00:00           # Time limit (hh:mm:ss)
 #SBATCH --mail-user=raphael.sulzer.1@gmail.com  # Email for notifications
 #SBATCH --mail-type=ALL           # When to receive emails (BEGIN, END, FAIL, ALL)
@@ -21,9 +21,10 @@ module load arch/a100
 module load miniforge/24.9.0
 
 # Activate virtual environment (if needed)
-conda activate pix2poly
+conda activate ppp
 
 set -x
 
 # Run your Python script
-torchrun --nproc_per_node=4 train.py log_to_wandb=true host=jz run_type=release multi_gpu=true dataset=lidarpoly experiment_name=lidarpoly_ori_aug checkpoint=null model.batch_size=32
+torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true dataset=lidarpoly \
+experiment_name=image_only_bs2x16 checkpoint=null model.batch_size=16 use_lidar=False use_images=True
