e7cf413f4e17f898a86c343aa07cb262a0f70e0b
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-03-19 19:52:45 +0100

    comment out vert num eval

--- slurm/job.sh
+++ slurm/job.sh
@@ -29,6 +29,3 @@ set -x
 # Run your Python script
 torchrun --nproc_per_node=4 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true dataset=lidarpoly \
 experiment_name=image_only_bs4x16 checkpoint=null model.batch_size=16 use_lidar=False use_images=True
-
-torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true dataset=lidarpoly \
-experiment_name=lidarpoly_D4_aug_bs2x16 checkpoint=backup model.batch_size=8 use_lidar=False use_images=True run_type=debug log_to_wandb=false
