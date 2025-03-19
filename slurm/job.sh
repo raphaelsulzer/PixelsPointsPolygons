07ad2e62dd8be24be282fbd8cc86f57e8d2192aa
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-03-19 22:35:55 +0100

    num workers to zero and pin memory false for debug

--- slurm/job.sh
+++ slurm/job.sh
@@ -29,3 +29,7 @@ set -x
 # Run your Python script
 torchrun --nproc_per_node=4 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true dataset=lidarpoly \
 experiment_name=image_only_bs4x16 checkpoint=null model.batch_size=16 use_lidar=False use_images=True
+
+module load miniforge/24.9.0 && conda activate ppp
+torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=jz run_type=release multi_gpu=true dataset=lidarpoly \
+experiment_name=debug checkpoint=null model.batch_size=16 use_lidar=true use_images=false run_type=debug log_to_wandb=false
