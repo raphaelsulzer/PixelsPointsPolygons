d4d02b552bc97d9d7f9ffc9f20f97a748e3d01d7
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-03-20 15:25:50 +0100

    add fusion slurm script

--- slurm/job_fusion.sh
+++ slurm/job_fusion.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --account=cso@h100
 #SBATCH --constraint=h100
-#SBATCH --job-name=lidar_only_bs2x32  # Job name
-#SBATCH --output=./slurm/h100.log       # Standard output and error log
-#SBATCH --error=./slurm/h100.log         # Error log
+#SBATCH --job-name=fusion_simple_bs4x32  # Job name
+#SBATCH --output=./slurm/h100_fusion.log       # Standard output and error log
+#SBATCH --error=./slurm/h100_fusion.log         # Error log
 #SBATCH --nodes=1 # reserve 1 node
-#SBATCH --ntasks=2 # reserve 4 tasks (or processes)
-#SBATCH --gres=gpu:2              # Request 2 GPUs
+#SBATCH --ntasks=4 # reserve 4 tasks (or processes)
+#SBATCH --gres=gpu:4              # Request 2 GPUs
 ##SBATCH --constraint v100-32g
 #SBATCH --cpus-per-task=16         # Request 8 CPU cores
 #SBATCH --qos=qos_gpu_h100-t3 # QoS
@@ -15,8 +15,8 @@
 #SBATCH --mail-user=raphael.sulzer.1@gmail.com  # Email for notifications
 #SBATCH --mail-type=ALL           # When to receive emails (BEGIN, END, FAIL, ALL)
 
-#hostname
-#nvidia-smi
+hostname
+nvidia-smi
 module purge # purge modules inherited by default
 #conda deactivate
 
@@ -31,5 +31,5 @@ set -x
 
 # Run your Python script
 
-torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=jz run_type=release multi_gpu=true dataset=lidarpoly \
-experiment_name=lidar_only_bs2x32 checkpoint=validation_best model.batch_size=32 use_lidar=True use_images=False
+torchrun --nproc_per_node=4 scripts/train.py log_to_wandb=true host=jz run_type=release multi_gpu=true dataset=lidarpoly \
+experiment_name=fusion_simple_bs4x32 checkpoint=null model.batch_size=32 use_lidar=True use_images=True
