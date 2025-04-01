ece25cbeec16baa7d4f006ded8eb222a2eebd878
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-04-01 15:12:59 +0200

    a lot of refactoring for predictor and trainer class

--- pixelspointspolygons/misc/logger.py
+++ pixelspointspolygons/misc/logger.py
@@ -1,4 +1,44 @@
-import logging, colorlog, sys
+import logging
+import colorlog
+import sys
+import torch
+
+from collections import defaultdict
+
+from .utils import SmoothedValue
+
+
+class MetricLogger:
+    def __init__(self, delimiter="\t"):
+        self.meters = defaultdict(SmoothedValue)
+        self.delimiter = delimiter
+
+    def update(self, **kwargs):
+        for k, v in kwargs.items():
+            if isinstance(v, torch.Tensor):
+                v = v.item()
+            assert isinstance(v, (float, int))
+            self.meters[k].update(v)
+
+    def __getattr__(self, attr):
+        if attr in self.meters:
+            return self.meters[attr]
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        raise AttributeError("'{}' object has no attribute '{}'".format(
+                    type(self).__name__, attr))
+
+    def __str__(self):
+        loss_str = []
+        keys = sorted(self.meters)
+        # for name, meter in self.meters.items():
+        for name in keys:
+            meter = self.meters[name]
+            loss_str.append(
+                "{}: {:.4f} ({:.4f})".format(name, meter.median, meter.global_avg)
+            )
+        return self.delimiter.join(loss_str)
+
 
 def make_logger(name="MyLogger",level=logging.INFO,
                 filepath=None):
---
+++ pixelspointspolygons/models/pix2poly/__init__.py
@@ -0,0 +1,2 @@
+from .model_pix2poly import LiDAREncoder, ImageEncoder, MultiEncoder, EncoderDecoder, Decoder
+from .tokenizer import Tokenizer
