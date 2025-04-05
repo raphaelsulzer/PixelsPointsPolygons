df71bdaf4dad2b02d1f57c505a2e19d74db1ec8c
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-04-06 01:55:44 +0200

    Update

--- oar/job_image.sh
+++ oar/job_image.sh
@@ -3,8 +3,8 @@
 #OAR -q production 
 #OAR -l host=1/gpu=2,walltime=16
 #OAR -p gpu-24GB AND gpu_compute_capability_major>=5
-#OAR -O oar/runs/%jobid%.out
-#OAR -E oar/runs/%jobid%.err 
+#OAR -O oar/runs/image.out
+#OAR -E oar/runs/image.err 
 
 # display some information about attributed resources
 hostname 
@@ -20,7 +20,7 @@ cd ./pixelspointspolygons/models/hisup/afm_module
 make
 cd ../../../../
 
-torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true experiment_name=image_bs2x16 checkpoint=null model.batch_size=8 use_lidar=False use_images=True
+torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true experiment_name=image_bs2x8 checkpoint=null model.batch_size=8 use_lidar=false use_images=true
 
 # python scripts/train.py log_to_wandb=false host=g5k run_type=debug multi_gpu=false experiment_name=debug checkpoint=null model.batch_size=16 use_lidar=False use_images=True
 
--- oar/job_lidar.sh
+++ oar/job_lidar.sh
@@ -3,8 +3,8 @@
 #OAR -q production 
 #OAR -l host=1/gpu=2,walltime=16
 #OAR -p gpu-24GB AND gpu_compute_capability_major>=5
-#OAR -O oar/runs/%jobid%.out
-#OAR -E oar/runs/%jobid%.err 
+#OAR -O oar/runs/lidar.out
+#OAR -E oar/runs/lidar.err 
 
 # display some information about attributed resources
 hostname 
@@ -20,5 +20,5 @@ cd ./pixelspointspolygons/models/hisup/afm_module
 make
 cd ../../../../
 
-torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true experiment_name=lidar_only_bs2x8 checkpoint=null model.batch_size=8 use_lidar=True use_images=False
+torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true experiment_name=lidar_bs2x8 checkpoint=null model.batch_size=8 use_lidar=true use_images=false
 
