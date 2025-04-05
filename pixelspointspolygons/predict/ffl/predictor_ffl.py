38af604b91acfb715464783d14d892540724f165
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-04-06 00:09:45 +0200

    Update

--- pixelspointspolygons/predict/ffl/predictor_ffl.py
+++ pixelspointspolygons/predict/ffl/predictor_ffl.py
@@ -74,13 +74,27 @@ class FFLPredictor(Predictor):
             
             
             if self.cfg.multi_gpu:
-                """make a single batch from the DDP batch"""
-                for k in batch.keys():
-                    temp = [None] * self.world_size
-                    self.logger.debug(f"Gathering {k} : {type(batch[k])}")
-                    dist.all_gather(temp, batch[k])
-                    batch[k] = torch.stack([item for sublist in temp for item in sublist])
                 
+                # for k in batch.keys():
+                #     temp = [None] * self.workd_size
+                #     dist.all_gather(temp, batch[k])
+                #     batch[k] = torch.stack([item for sublist in temp for item in sublist])
+                
+                # Gather the list of dictionaries from all ranks
+                seg = [None] * self.world_size  # Placeholder for gathered objects
+                dist.all_gather_object(seg, batch["seg"])
+
+                # Flatten the list of lists into a single list
+                batch["seg"] = torch.stack([item for sublist in seg for item in sublist])
+                
+                # Gather the list of dictionaries from all ranks
+                crossfield = [None] * self.world_size  # Placeholder for gathered objects
+                dist.all_gather_object(crossfield, batch["crossfield"])
+
+                # Flatten the list of lists into a single list
+                batch["crossfield"] = torch.stack([item for sublist in crossfield for item in sublist])
+                
+            
             if self.local_rank == 0:
                 # --- Polygonize
                 try:
