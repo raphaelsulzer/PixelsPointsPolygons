c00860f4df9e95c9312c9e6aea51eebce872f204
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-04-09 00:21:37 +0200

    add Pointpillars installataion to oar

--- oar/job_image.sh
+++ oar/job_image.sh
@@ -20,6 +20,13 @@ cd ./pixelspointspolygons/models/hisup/afm_module
 make
 cd ../../../../
 
+# install pointpillars
+cd ../PointPillars
+python setup.py build_ext --inplace
+pip install .
+cd ../PixelsPointPolygons
+
+
 torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true experiment_name=image_hisup_ori_bs2x12 checkpoint=null model.batch_size=12 encoder=hrnet
 
 # python scripts/train.py log_to_wandb=false host=g5k run_type=debug multi_gpu=false experiment_name=debug checkpoint=null model.batch_size=16 use_lidar=False use_images=True
