68b28fcba5cd659ddbd7dbb178c2d6869f8bcf82
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-04-08 15:45:01 +0200

    Update

--- oar/job_image.sh
+++ oar/job_image.sh
@@ -4,7 +4,7 @@
 #OAR -l host=1/gpu=2,walltime=16
 #OAR -p gpu-24GB AND gpu_compute_capability_major>=5
 #OAR -O oar/runs/image.out
-#OAR -E oar/runs/image.err 
+#OAR -E oar/runs/image.out
 
 # display some information about attributed resources
 hostname 
@@ -20,7 +20,7 @@ cd ./pixelspointspolygons/models/hisup/afm_module
 make
 cd ../../../../
 
-torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true experiment_name=image_bs2x8 checkpoint=null model.batch_size=8 use_lidar=false use_images=true
+torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true experiment_name=image_bs2x12 checkpoint=null model.batch_size=12 encoder=hrnet
 
 # python scripts/train.py log_to_wandb=false host=g5k run_type=debug multi_gpu=false experiment_name=debug checkpoint=null model.batch_size=16 use_lidar=False use_images=True
 
