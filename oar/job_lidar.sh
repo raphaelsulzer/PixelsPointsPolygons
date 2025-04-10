98f66c399693acc014af180904fb5cb3a4b296ab
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-04-10 18:10:53 +0200

    update oar

--- oar/job_lidar.sh
+++ oar/job_lidar.sh
@@ -3,8 +3,8 @@
 #OAR -q production 
 #OAR -l host=1/gpu=2,walltime=16
 #OAR -p gpu-24GB AND gpu_compute_capability_major>=5
-#OAR -O oar/runs/lidar_pp_ori.out
-#OAR -E oar/runs/lidar_pp_ori.out 
+#OAR -O oar/runs/lidar_pp_o3d.out
+#OAR -E oar/runs/lidar_pp_o3d.out 
 
 ## display some information about attributed resources
 hostname 
@@ -28,5 +28,5 @@ cd ../../../../
 # pip install .
 # cd ../PixelsPointPolygons
 
-torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true experiment_name=v3_lidar_ppori_vit_cnn_bs2x12 checkpoint=null model.batch_size=12 encoder=pointpillars_vit_cnn
+torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true experiment_name=v3_lidar_pp_o3d_bs2x16 checkpoint=null model.batch_size=16 encoder=pointpillars
 
