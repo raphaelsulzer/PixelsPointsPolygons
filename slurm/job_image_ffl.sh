b1234df9fbc8f1423a92adb80c75fcd81111cdae
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-04-24 18:10:26 +0200

    update ffl default batch size to 16

--- slurm/job_image_ffl.sh
+++ slurm/job_image_ffl.sh
@@ -34,4 +34,4 @@ set -x
 
 # Run your Python script
 
-torchrun --nproc_per_node=4 scripts/train.py log_to_wandb=true host=jz run_type=release multi_gpu=true checkpoint=null model.batch_size=16 experiment_name=v3_image_vit_cnn_bs4x16 model=ffl encoder=vit_cnn 
+torchrun --nproc_per_node=4 scripts/train.py log_to_wandb=true host=jz run_type=release multi_gpu=true checkpoint=null experiment=ffl_image
