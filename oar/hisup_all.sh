290c8539a639d73ddbc84ea22979153c5f738f02
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-05-02 13:49:21 +0200

    added jobs for all

---
+++ oar/hisup_all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+#OAR -q production 
+#OAR -l host=1/gpu=2,walltime=48
+#OAR -p gpu-24GB AND gpu_compute_capability_major>=5
+#OAR -O oar/runs/hisup_all.out
+#OAR -E oar/runs/hisup_all.out
+#OAR -n hisup_all
+
+# display some information about attributed resources
+hostname 
+nvidia-smi 
+
+# make use of a python torch environment
+module load conda
+conda activate ppp
+python3 -c "import torch; print(torch.cuda.is_available()); print(torch.cuda.get_device_name(0))";
+
+# to get rid of this annoying warning: sh: /home/rsulzer/.conda/envs/ppp/bin/../lib/libtinfo.so.6: no version information available (required by sh)
+export LD_BIND_NOW=1
+# recompile the afm module
+cd ./pixelspointspolygons/models/hisup/afm_module
+make
+cd ../../../../
+
+torchrun --nproc_per_node=2 scripts/train.py log_to_wandb=true host=g5k run_type=release multi_gpu=true checkpoint=null experiment=hisup_fusion experiment.name=v0_all_bs2x16 country=all
