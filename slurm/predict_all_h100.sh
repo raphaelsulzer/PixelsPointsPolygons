1ee6fa02b42e3c1439267bc3ec347fe137175139
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-05-08 12:46:17 +0200

    slurm

--- slurm/predict_all_h100.sh
+++ slurm/predict_all_h100.sh
@@ -34,4 +34,4 @@ set -x
 
 # Run your Python script
 
-torchrun --nproc_per_node=4 scripts/all_countries.py host=jz run_type=release multi_gpu=true checkpoint=best_val_iou country=all
+torchrun --nproc_per_node=4 scripts/all_countries.py host=jz run_type=release multi_gpu=true checkpoint=best_val_iou country=all eval.split=test
