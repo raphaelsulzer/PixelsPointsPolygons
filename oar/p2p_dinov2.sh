c5bb20d000844f5fc100442094afe97fb22d6daa
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-07-29 17:47:31 +0200

    Update

---
+++ oar/p2p_dinov2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+#OAR -q production 
+#OAR -l host=1/gpu=2,walltime=24
+#OAR -p gpu-24GB AND gpu_compute_capability_major>=5
+#OAR -O oar/runs/p2p_dinov2.out
+#OAR -E oar/runs/p2p_dinov2.out
+#OAR -n p2p_dinov2
+
+# display some information about attributed resources
+hostname 
+nvidia-smi 
+
+# make use of a python torch environment
+module load conda
+conda activate ppp
+python3 -c "import torch; print(torch.cuda.is_available()); print(torch.cuda.get_device_name(0))";
+
+# to get rid of this annoying warning: sh: /home/rsulzer/.conda/envs/ppp/bin/../lib/libtinfo.so.6: no version information available (required by sh)
+export LD_BIND_NOW=1
+# recompile the afm module
+cd ./pixelspointspolygons/models/hisup/afm_module
+make
+cd ../../../../
+
+torchrun --nproc_per_node=2 scripts/train.py run_type=release host=g5k experiment=p2p_image_dinov2 experiment.name=p2p_image_dinov2_bs2x16
