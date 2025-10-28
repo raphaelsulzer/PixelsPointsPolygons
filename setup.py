7d66e1015294f7039c29ba3b8e5fe99244bc0d9f
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-10-28 12:52:47 +0100

    add cuda_runtime.h hack to voxel_op setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+import os
+
+### very hacky way to get rid of "cc1plus: fatal error: cuda_runtime.h: No such file or directory" but nothing else worked :(
+cuda_home = os.getenv("CONDA_PREFIX", "/usr/local/cuda")
+print("\nSET CUDA_HOME TO: ", cuda_home)
+include_dirs = [os.path.join(cuda_home, "targets/x86_64-linux/include"),
+                os.path.join(cuda_home, "lib/python3.11/site-packages/nvidia/cuda_runtime/include")]
 
 setup(
     name='pixelspointspolygons',
@@ -22,7 +29,8 @@ setup(
                     '--expt-relaxed-constexpr',
                     '--allow-unsupported-compiler'
                 ],
-            }
+            },
+            include_dirs=include_dirs
         ),
     ],
     cmdclass={'build_ext': BuildExtension},
